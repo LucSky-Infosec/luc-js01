c75247796f18f2ae6b49e8c5748d9ead01a7c6a0
LucSky-Infosec <94608408+LucSky-Infosec@users.noreply.github.com>
2023-11-15 17:47:24 +0700

    Add files via upload
    Exercise3

---
+++ 2dArrays.js
@@ -0,0 +1,20 @@
+var arr = [
+    [0,1,1],
+    [0,1,1],
+    [0,1,1],
+    [0,0,1]
+];
+var row=3
+var col=4
+var len=arr.length
+var line1=""
+var line2=""
+var line3=""
+for (let i=0;i<len;i++){
+    line1+=arr[i][0] + " "
+    line2+=arr[i][1] + " "
+    line3+=arr[i][2] + " "
+}
+console.log(t)
+console.log(line1 + "\n"+line2+"\n"+line3)
+console.log(arr)
