ee0cd20733b630f1ef6d79243c1c42297955752d
LucSky-Infosec <94608408+LucSky-Infosec@users.noreply.github.com>
2023-11-15 17:55:58 +0700

    Exercise2

--- 2.getRandomInteger.js
+++ 2.getRandomInteger.js
@@ -1,5 +1,5 @@
 function randomNumber(min, max){
-    if (max<min){return false}
+    if (max<min){return false} 
     return Math.round(Math.floor(Math.random()*(max-min)+min))
 }
 console.log(randomNumber(5.8,15))
